fb1b2baf78b9da21db505b6f164f238bd26ee041
Harsh Jaiswal <hjaiswal0220@gmail.com>
2026-07-24 16:23:10 +0530

    Debug Mongo URL

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,6 +3,8 @@ from app.core.config import settings
 
 
 async def connect_to_mongo(app):
+    print("Mongo URL repr:", repr(settings.MONGODB_URL))
+    print("Starts with mongodb:", settings.MONGODB_URL.startswith("mongodb"))
     client = AsyncIOMotorClient(settings.MONGODB_URL)
     app.state.mongo_client = client
     app.state.db = client["ai_legal_sentinel"]
